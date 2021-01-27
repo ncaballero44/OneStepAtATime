095326959972237067fedfc51791a05b5bdfb99b
Nicholas Caballero <ncaballero9001@gmail.com>
2021-01-26 18:58:54 -0800

    Project Creation

---
+++ app/src/main/java/com/example/onestepatatime/ClientMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.onestepatatime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientMainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
