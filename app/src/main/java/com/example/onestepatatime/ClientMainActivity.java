2a5491281eb1411836d0e4d81513f65dd2b51439
Nicholas Caballero <ncaballero9001@gmail.com>
2021-01-26 19:31:50 -0800

    Started on the main client page

--- app/src/main/java/com/example/onestepatatime/ClientMainActivity.java
+++ app/src/main/java/com/example/onestepatatime/ClientMainActivity.java
@@ -3,14 +3,21 @@ package com.example.onestepatatime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ClientMainActivity extends AppCompatActivity
 {
+    Button calendarButton;
+    Button journalButton;
+    Button worksheetsButton;
+    Button notesButton;
+    Button therapistListButton;
+    Button emergencyContactListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.client_main_activity);
     }
 }
