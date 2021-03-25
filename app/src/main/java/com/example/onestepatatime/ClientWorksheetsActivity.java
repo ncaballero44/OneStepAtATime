25200e7123ce6edfd49a2518688765d8e48e2ad8
Nicholas Caballero <ncaballero9001@gmail.com>
2021-03-24 20:02:10 -0700

    Able to save to download folder of shared storage.

--- app/src/main/java/com/example/onestepatatime/ClientWorksheetsActivity.java
+++ app/src/main/java/com/example/onestepatatime/ClientWorksheetsActivity.java
@@ -1,17 +1,61 @@
 package com.example.onestepatatime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ClientWorksheetsActivity extends AppCompatActivity
 {
+    private static final int STORAGE_PERMISSION_CODE=100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_worksheets_activity);
+
+       if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED)
+       {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
+       }
+       else
+       {
+           try
+           {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.MediaColumns.DISPLAY_NAME, "test");
+                values.put(MediaStore.MediaColumns.MIME_TYPE, "text/plain");
+                values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS + "/One Step At A Time Shared Files");
+
+                Uri uri=getContentResolver().insert(MediaStore.Downloads.getContentUri("external"),values);
+
+                OutputStream outputStream=getContentResolver().openOutputStream(uri);
+                outputStream.write("This is a test.".getBytes());
+                outputStream.close();
+                Toast.makeText(this, "File created", Toast.LENGTH_SHORT).show();
+           }
+           catch (FileNotFoundException e)
+           {
+               Toast.makeText(this, "File not found", Toast.LENGTH_SHORT).show();
+           }
+           catch (IOException e)
+           {
+               Toast.makeText(this, "Failed to create file", Toast.LENGTH_SHORT).show();
+           }
+       }
+
     }
 }
