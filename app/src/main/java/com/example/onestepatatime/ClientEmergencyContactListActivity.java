1cf247e6b12a6174760dc50dee3b0ed856b33385
Nicholas Caballero <ncaballero9001@gmail.com>
2021-04-27 18:22:46 -0700

    Created the aesthetic layout for adding a new emergency contact.

--- app/src/main/java/com/example/onestepatatime/ClientEmergencyContactListActivity.java
+++ app/src/main/java/com/example/onestepatatime/ClientEmergencyContactListActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class ClientEmergencyContactListActivity extends AppCompatActivity
 {
+    ListView emergencyContactList;
+    Button addNewContactButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_emergency_contact_list_activity);
+        initializeElements();
+    }
+
+    private void initializeElements()
+    {
+        this.emergencyContactList=findViewById(R.id.emergencyContactList);
+        this.addNewContactButton=findViewById(R.id.addNewContactButton);
+
+        this.addNewContactButton.setOnClickListener((view)->{
+            startActivity(new Intent(ClientEmergencyContactListActivity.this,ClientAddNewEmergencyContactEntryActivity.class));
+        });
     }
 }
